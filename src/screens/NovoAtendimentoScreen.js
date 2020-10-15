695d1091099873c0a711a9275f4e037149170132
renato <renatodmagrin@gmail.com>
2020-10-14 23:27:55 -0300

    terminando tela do novo atendimento

--- src/screens/NovoAtendimentoScreen.js
+++ src/screens/NovoAtendimentoScreen.js
@@ -5,25 +5,38 @@ import {
   Text,
   ProgressBar,
   Colors,
+  TextInput,
+  Button,
 } from 'react-native-paper';
 import { getResults } from '../controllers/Login';
 import { theme } from '../../styles';
 
 export default function NovoAtendimento({ navigation, route }) {
   const { item } = route.params;
-  const [selectedValue, setSelectedValue] = useState('java');
+  const [novaEvolucao, setNovaEvolucao] = React.useState('');
+  const [resumo, setResumo] = React.useState('');
+  const [atendimento, setAtendimento] = React.useState('');
   const progressoEvolucao = item.evolucao / 100;
 
-  console.log(item);
-  console.log(item.evolucao / 100);
+  const salvar = () => {
+    if (novaEvolucao == '') {
+      console.log('evolucao ta em branco');
+    }
+    if (resumo == '') {
+      console.log('resumo ta em branco');
+    }
+    if (atendimento == '') {
+      console.log('atendimento ta em branco');
+    }
+  };
 
   return (
     <PaperProvider theme={theme}>
       <View style={styles.viewTitle}>
-        <Text style={styles.title}>Historico de Atendimento</Text>
+        <Text style={styles.title}>Novo Atendimento</Text>
       </View>
       <View style={styles.container}>
-        <Text>Evolução:</Text>
+        <Text>Evolução atual: {item.evolucao} </Text>
       </View>
       <View>
         {progressoEvolucao <= 0.5 ? (
@@ -32,15 +45,32 @@ export default function NovoAtendimento({ navigation, route }) {
           <ProgressBar progress={item.evolucao / 100} color={Colors.green800} />
         )}
       </View>
-      <View style={styles.container}>
-        <Picker
-          selectedValue={selectedValue}
-          style={{ height: 50, width: 150 }}
-          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-        >
-          <Picker.Item label="Java" value="java" />
-          <Picker.Item label="JavaScript" value="js" />
-        </Picker>
+      <View style={styles.textEvolucao}>
+        <TextInput
+          label="evolução"
+          value={novaEvolucao}
+          onChangeText={(novaEvolucao) => setNovaEvolucao(novaEvolucao)}
+          keyboardType="number-pad"
+        />
+      </View>
+      <View style={styles.textTittle}>
+        <TextInput
+          label="Resumo"
+          value={resumo}
+          onChangeText={(resumo) => setResumo(resumo)}
+        />
+      </View>
+      <View style={styles.textAtendimento}>
+        <TextInput
+          label="Atendimneto"
+          value={atendimento}
+          onChangeText={(atendimento) => setAtendimento(atendimento)}
+        />
+      </View>
+      <View style={styles.btn}>
+        <Button icon="file-upload" mode="outlined" onPress={salvar}>
+          Salvar
+        </Button>
       </View>
     </PaperProvider>
   );
@@ -51,7 +81,26 @@ const styles = StyleSheet.create({
     marginTop: 10,
     alignItems: 'center',
   },
-
+  textEvolucao: {
+    marginTop: 25,
+    paddingLeft: 150,
+    paddingRight: 150,
+  },
+  textTittle: {
+    marginTop: 25,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  textAtendimento: {
+    marginTop: 25,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  btn: {
+    marginTop: 30,
+    paddingLeft: 120,
+    paddingRight: 120,
+  },
   fontBold: {
     fontWeight: 'bold',
   },
